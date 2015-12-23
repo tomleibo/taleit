ef0c48e68edbf5b46d648e272e458ed7d53d3db3
sharon kerzman <sharonk@wix.com>
2015-12-23 21:14:10 +0200

    added fields to enum

--- Server/src/test/java/usecases/UserDetailForTest.java
+++ Server/src/test/java/usecases/UserDetailForTest.java
@@ -4,10 +4,15 @@ package usecases;
  * Created by sharonk on 12/23/2015
  */
 public enum UserDetailForTest {
+    // usernames
     USERNAME_VALID("sharon@gmail.com"),
     USERNAME_INVALID("sharon"),
+    USERNAME_FIRST_TIME("shairip@homale.com"),
+
+    // passwords
     PASSWORD_VALID("123abc"),
-    PASSWORD_TO_SHORT("123a");
+    PASSWORD_INCORRECT("abc123"),
+    PASSWORD_TO_SHORT("123a"),;
 
     private final String value;
 
