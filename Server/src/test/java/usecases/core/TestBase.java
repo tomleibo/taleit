2c4b35702eebdc6b0c9ab37b936f345b1f58aebf
sharon kerzman <sharonk@wix.com>
2016-01-15 21:47:31 +0200

    pulled up userName and Passport to base

--- Server/src/test/java/usecases/core/TestBase.java
+++ Server/src/test/java/usecases/core/TestBase.java
@@ -1,10 +1,14 @@
 package usecases.core;
 
 import model.Model;
+import usecases.utils.UserDetailForTest;
 
 /**
  * Created by Kerzman on 12/23/2015.
  */
 public class TestBase {
+    protected String userName = UserDetailForTest.USERNAME_VALID.getValue();
+    protected String password = UserDetailForTest.PASSWORD_VALID.getValue();
+
     public Model model = new Model();
 }
