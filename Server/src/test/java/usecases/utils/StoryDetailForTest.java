c4a9895e94e7385715464f0d9fa4eed15e6cb06f
sharon kerzman <sharonk@wix.com>
2016-01-15 19:54:11 +0200

    added paragraph to enum

--- Server/src/test/java/usecases/utils/StoryDetailForTest.java
+++ Server/src/test/java/usecases/utils/StoryDetailForTest.java
@@ -1,13 +1,17 @@
 package usecases.utils;
 
+import model.Paragraph;
+
 /**
  * Created by Kerzman on 12/23/2015
  */
 public enum StoryDetailForTest {
-    // usernames
     AUTHOR("B.J. Rollin"),
     TITLE("True Detective"),
-    BODY("Once upon a time i was a girl");
+    BODY("Once upon a time i was a girl"),
+
+    PARAGRAPH_TEXT("and it feels great"),
+    PARAGRAPH_TEXT_SECOND("and it feels awful");
     private final String value;
 
     StoryDetailForTest(String value) {
