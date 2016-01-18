f17b2c8795537bdc3a3c2efef84bdab2e5ac6ba9
sharon kerzman <sharonk@wix.com>
2016-01-18 16:27:26 +0200

    added assertions to createStoryAcceptance

--- Server/src/test/java/acceptance/tests/CreateStoryAcceptance.java
+++ Server/src/test/java/acceptance/tests/CreateStoryAcceptance.java
@@ -4,6 +4,7 @@ import acceptance.core.LoggedInBaseAcceptance;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
@@ -21,18 +22,21 @@ public class CreateStoryAcceptance extends LoggedInBaseAcceptance {
     public void validStroy() {
         storyNumber = bridge.createStory(storyTitle, storyText);
         assertFalse("Could not create a story with Title: " + storyTitle + " and body: " + storyText, storyNumber == 0);
+        assertTrue("Story is not exists", bridge.isStoryExists(storyNumber));
     }
 
     @Test
     public void noTitle() {
         storyNumber = bridge.createStory("", storyText);
         assertTrue("succeeded to create a story with no title", storyNumber == 0);
+        assertFalse("Story exists with no title", bridge.isStoryExists(storyNumber));
     }
 
     @Test
     public void noText() {
         storyNumber = bridge.createStory(storyTitle, "");
         assertTrue("succeeded to create a story with no text", storyNumber == 0);
+        assertFalse("Story exists with no text", bridge.isStoryExists(storyNumber));
     }
 
     @Test
@@ -50,6 +54,7 @@ public class CreateStoryAcceptance extends LoggedInBaseAcceptance {
         // assertions
         for (int i=0; i<randomNumber; i++){
             assertFalse("Could not create a story number " + i+1 , storiesNumber[i]== 0);
+            assertTrue("Story numer" + i+1 + "is not exists", bridge.isStoryExists(storiesNumber[i]));
         }
     }
 
