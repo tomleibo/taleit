0a74f2a24dbced3e600850f7a47b7520cbb67b2a
sharon kerzman <sharonk@wix.com>
2016-05-24 17:54:19 +0300

    added link to new category page

--- Server/src/main/webapp/js/app.js
+++ Server/src/main/webapp/js/app.js
@@ -1,50 +1,65 @@
-
-(function() {
+(function () {
     var app = angular.module('taleItApp', ['ui.bootstrap']);
 
-    app.controller('CategoriesController', ['$http', function($http){
+    app.controller('CategoriesController', ['$http', function ($http) {
         var catList = this;
         catList.categoriesResult = [];
 
-        $http.get('http://127.0.0.1:8080/rest/stories/categories').success(function(data){
+        $http.get('http://127.0.0.1:8080/rest/stories/categories').success(function (data) {
             catList.categoriesResult = data;
         });
     }]);
 
-    app.directive("categoriesSection", function() {
+    app.directive("categoriesSection", function () {
         return {
             restrict: "E",
             templateUrl: "/html/categories-section.html"
         };
     });
 
-    app.controller('StoriesController', ['$http', function($http){
+    app.controller('StoriesController', ['$http', '$location', function ($http, $location) {
+        this.getURLParameter = function(sParam) {
+            var sPageURL = window.location.search.substring(1);
+            var sURLVariables = sPageURL.split('&');
+            for (var i = 0; i < sURLVariables.length; i++) {
+                var sParameterName = sURLVariables[i].split('=');
+                if (sParameterName[0] == sParam) {
+                    return sParameterName[1];
+                }
+            }
+            return null;
+        }
+
         var storiesList = this;
-
         var browseUrl = 'http://127.0.0.1:8080/rest/stories/browse';
+
         storiesList.storiesResult = [];
-        storiesList.storyByCategoryList = [];
 
-        $http.get(browseUrl).success(function(data){
-            storiesList.storiesResult = data;
-        });
+        var searchResults = this.getURLParameter('category');
 
-        this.getStoriesFromCategory = function (categoryStr){
-            var postFixUrl = '?category='+categoryStr;
-            $http.get(browseUrl+postFixUrl).success(function(data){
+        if (searchResults === null) {
+            $http.get(browseUrl).success(function (data) {
                 storiesList.storiesResult = data;
             });
         }
+        else {
+            var postFixUrl = '?category=' + searchResults;
+            $http.get(browseUrl + postFixUrl).success(function (data) {
+                storiesList.storiesResult = data;
+            });
+        }
+
+
     }]);
 
-    app.directive("storiesSection", ['$http', function() {
+    app.directive("storiesSection", ['$http', function () {
         return {
             restrict: "E",
             templateUrl: "/html/stories-section.html"
         };
     }]);
 
-    app.directive("titleSection", function() {
+    app.directive("titleSection", function () {
         return {
             restrict: "E",
             templateUrl: "/html/title-section.html"
