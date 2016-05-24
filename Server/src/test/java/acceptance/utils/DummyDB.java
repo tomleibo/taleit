f88e90bb0871fee317289d53a56024ba1374af6d
shaiRip <shairip@gmail.com>
2016-05-24 13:15:27 +0300

    dummy DB done

--- Server/src/test/java/acceptance/utils/DummyDB.java
+++ Server/src/test/java/acceptance/utils/DummyDB.java
@@ -1,37 +1,31 @@
 package acceptance.utils;
 
 import acceptance.core.LoggedInBaseAcceptance;
-import ioc.Server;
-import model.Model;
-import org.junit.Before;
+import model.Categories;
 import org.junit.Test;
 
-import static junit.framework.Assert.*;
 
 /**
  * Created by sharonk on 1/18/2016
  */
 public class DummyDB extends LoggedInBaseAcceptance {
     String storyNumber;
+//story title, root title, root text
+    public static String[][] stories = {
+    {"Dumb and more dumb", "the rubber chicken", "He looked in the chicken eyes and kissed it gently", Categories.COMEDY.getValue()},
+    {"King of the humus", "She wasn't the prettiest", "She gave me the puppy eyes in a gas station", Categories.COMEDY.getValue()},
+    {"Timetravel is a bitch", "past and present", "Hold the door! Hold the door, holddoor, hodor...", Categories.DRAMA.getValue()},
+    {"To kill to drink to die", "vodka is my only lover", "As a strong independent women who needed no men in here life, I was lonely", Categories.DRAMA.getValue()},
+    {"Ender's new game", "Chess", "chess in space is not so fun", Categories.SCIFI.getValue()},
+    {"A biography of the first Nazi cleaning lady", "Intro", "It all started in 1942", Categories.OTHERS.getValue()}};
 
     @Test
     public void eraseInjectDummyDB() {
         super.init();
         bridge.signUp(userName, password);
         bridge.login(userName,password);
-        storyNumber = bridge.createStory(storyTitle, storyTitle, storyText, "");
-
-
-
-
-
-
-
-
-
-
-
+        for (String[] story : stories) {
+            bridge.createStory(story[0], story[1], story[2], story[3]);
+        }
     }
-
-
 }
