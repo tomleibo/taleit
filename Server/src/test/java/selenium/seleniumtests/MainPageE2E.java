a994f28e89b0dcdf29d6ca66e307535578ffe2e3
shaiRip <shairip@gmail.com>
2016-06-08 00:16:15 +0300

    added a test

--- Server/src/test/java/selenium/seleniumtests/MainPageE2E.java
+++ Server/src/test/java/selenium/seleniumtests/MainPageE2E.java
@@ -1,9 +1,15 @@
 package selenium.seleniumtests;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import selenium.pageObjects.MainPageObject;
 import selenium.SeleniumBase;
+
+import java.util.List;
 
 /**
  * Created by Shai on 07/06/2016.
@@ -12,8 +18,39 @@ public class MainPageE2E extends SeleniumBase {
 
     @Test
     public void numberOfCategories(){
-        MainPageObject MainPageObject = facade.mainPageObject();
+        MainPageObject mainPageObject = facade.mainPageObject();
 
-        assertEquals(MainPageObject.categories().size(), 9);
+        assertEquals(mainPageObject.categories().size(), 9);
     }
+
+    @Test
+    public void imgChanges(){
+        MainPageObject mainPageObject = facade.mainPageObject();
+
+        List<WebElement> elems = mainPageObject.categoryImages();
+        String width = elems.get(0).getCssValue("width");
+        assertTrue(width.contains("160"));
+
+        Actions action = mainPageObject.getAction();
+        action.moveToElement(elems.get(0));
+        action.perform();
+        
+        mainPageObject.wait2secs();
+
+        elems = mainPageObject.categoryImages();
+        width = elems.get(0).getCssValue("width");
+        assertTrue(width.contains("220"));
+
+
+    }
+
+    public void sleep(int seconds)
+    {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
 }
